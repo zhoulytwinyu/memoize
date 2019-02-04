b85d5c0975df190c00c5928034c223ad1c801387
Lingyu Zhou <zly@TRLDSC2.CHBOSTON.ORG>
2019-02-04 14:48:43 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+javascript memoization util functions
---
+++ index.js
@@ -0,0 +1,47 @@
+export function memoize_all(fn) {
+  let cache = {};
+  return function(){
+    let argumentsJSON = JSON.stringify(arguments);
+    if (argumentsJSON in cache) {
+      return cache[argumentsJSON];
+    }
+    else {
+      cache[argumentsJSON] = fn(...arguments);
+      return cache[argumentsJSON];
+    }
+  }
+}
+
+export function memoize_one(fn) {
+  let cachedArgs = undefined;
+  let cachedRes = undefined;
+  return function(){
+    if (cachedArgs === undefined){
+      cachedArgs = [...arguments];
+      cachedRes = fn(...arguments);
+      return cachedRes;
+    }
+    
+    let newArgs = [...arguments];
+    if (testArrayShallowEqual(cachedArgs,newArgs)){
+      return cachedRes;
+    }
+    else {
+      cachedArgs = [...arguments];
+      cachedRes = fn(...arguments);
+      return cachedRes;
+    }
+  }
+}
+
+function testArrayShallowEqual(array1,array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i=0; i<array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
